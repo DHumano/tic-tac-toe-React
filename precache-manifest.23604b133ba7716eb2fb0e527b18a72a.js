3a67eebd1e008d8f604a98baf248db8b69931a34
dario <darioh.dev@gmail.com>
2020-04-23 13:54:00 -0300

    Updates

---
+++ precache-manifest.23604b133ba7716eb2fb0e527b18a72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6899cde7c3a5c06cfad154cf4c9c324",
+    "url": "/tic-tac-toe-React/index.html"
+  },
+  {
+    "revision": "3bd76a86abb582eb3bb2",
+    "url": "/tic-tac-toe-React/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "c907fc87b0165d7b18ec",
+    "url": "/tic-tac-toe-React/static/js/2.927740f4.chunk.js"
+  },
+  {
+    "revision": "3bd76a86abb582eb3bb2",
+    "url": "/tic-tac-toe-React/static/js/main.e55b7d18.chunk.js"
+  },
+  {
+    "revision": "4f2aab3f839f1066204f",
+    "url": "/tic-tac-toe-React/static/js/runtime~main.b56db4b5.js"
+  }
+]);
